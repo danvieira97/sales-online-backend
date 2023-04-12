e3be0dd3b0493e9ef5acf967c17e2f6dbde02a92
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-04-12 14:46:33 -0300

    feat: City module

---
+++ src/city/city.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CityEntity } from './entities/city.entity';
+import { Repository } from 'typeorm';
+import { CacheService } from 'src/cache/cache.service';
+
+@Injectable()
+export class CityService {
+  constructor(
+    @InjectRepository(CityEntity)
+    private readonly cityRepository: Repository<CityEntity>,
+
+    private readonly cacheService: CacheService,
+  ) {}
+
+  async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
+    return await this.cacheService.getCache<CityEntity[]>(
+      `state_${stateId}`,
+      () =>
+        this.cityRepository.find({
+          where: {
+            stateId,
+          },
+        }),
+    );
+  }
+}

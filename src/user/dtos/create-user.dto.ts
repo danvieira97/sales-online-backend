021ef41b04b1bb83c449ad6fa6a48ed15e00e698
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-04-12 15:12:39 -0300

    Add ReturnUserDTO and Pipes Validator

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,7 +1,18 @@
-export interface CreateUserDTO {
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
   cpf: string;
+
+  @IsString()
   phone: string;
+
+  @IsString()
   password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UserService } from './user.service';
+import { ReturnUserDTO } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(ValidationPipe)
   @Post()
   async createUser(@Body() createUserDTO: CreateUserDTO) {
     return await this.userService.createUser(createUserDTO);
@@ -13,6 +22,8 @@ export class UserController {
 
   @Get()
   async getAllUsers() {
-    return await this.userService.getAllUsers();
+    return (await this.userService.getAllUsers()).map(
+      (userEntity) => new ReturnUserDTO(userEntity),
+    );
   }
 }

15a45bf554f0e54b98d236ae9c9485c35df36ff6
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-04-14 23:19:03 -0300

    feat: Authorization, Roles Guard

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -8,11 +8,14 @@ import {
 } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDTO } from './dtos/create-address.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
+  @Roles(UserType.User)
   @Post('/:userId')
   @UsePipes(ValidationPipe)
   async createAddress(

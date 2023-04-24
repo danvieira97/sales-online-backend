4c05032b533bab7de8e8322f2b42efecc1a84d6c
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-04-24 14:16:34 -0300

    feat: Create unit test in User Service

---
+++ src/user/__tests__/user.service.spec.ts
@@ -0,0 +1,91 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { UserService } from '../user.service';
+import { userEntityMock } from '../__mocks__/user.mock';
+import { createUserMock } from '../__mocks__/createUser.mock';
+
+describe('UserService', () => {
+  let service: UserService;
+  let userRepository: Repository<UserEntity>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(userEntityMock),
+            save: jest.fn().mockResolvedValue(userEntityMock),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  it('should return user in getUserByEmail', async () => {
+    const user = await service.getUserByEmail(userEntityMock.email);
+
+    expect(user).toEqual(userEntityMock);
+  });
+
+  it('should return error in getUserByEmail', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(undefined);
+
+    expect(service.getUserByEmail(userEntityMock.email)).rejects.toThrowError();
+  });
+
+  it('should return error in getUserByEmail (error DB)', async () => {
+    jest.spyOn(userRepository, 'findOne').mockRejectedValueOnce(new Error());
+
+    expect(service.getUserByEmail(userEntityMock.email)).rejects.toThrowError();
+  });
+
+  it('should return user in getUserById', async () => {
+    const user = await service.getUserById(userEntityMock.id);
+
+    expect(user).toEqual(userEntityMock);
+  });
+
+  it('should return error in getUserById', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(undefined);
+
+    expect(service.getUserByEmail(userEntityMock.email)).rejects.toThrowError();
+  });
+
+  it('should return error in getUserById (error DB)', async () => {
+    jest.spyOn(userRepository, 'findOne').mockRejectedValueOnce(new Error());
+
+    expect(service.getUserById(userEntityMock.id)).rejects.toThrowError();
+  });
+
+  it('should return user in getUserByIdUsingRelations', async () => {
+    const user = await service.getUserByIdUsingRelations(userEntityMock.id);
+
+    expect(user).toEqual(userEntityMock);
+  });
+
+  it('should return error if user exist', async () => {
+    expect(service.createUser(createUserMock)).rejects.toThrowError();
+  });
+
+  it('should return user if user not exist', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(undefined);
+
+    const user = await service.createUser(createUserMock);
+
+    expect(user).toEqual(userEntityMock);
+  });
+});
